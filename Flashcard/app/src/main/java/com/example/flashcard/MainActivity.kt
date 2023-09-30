0355e7c650ef5ca89d2a1786bda12bffcc528c11
shangzhouyin <cxysz8246@163.com>
2023-09-30 15:25:55 -0400

    update login page

--- Flashcard/app/src/main/java/com/example/flashcard/MainActivity.kt
+++ Flashcard/app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.flashcard
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.flashcard.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var username : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.loginButton.setOnClickListener {
+            if(binding.username.text.toString() == "user" && binding.password.text.toString() == "123456"){
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
     }
 }
