6406c1c0bdeea16aa0758838c36b3742cecf4b34
ShuYang <yangshu2022@yahoo.com>
2023-10-02 22:05:47 -0400

    full version

---
+++ Flashcard/app/src/main/java/com/example/hw3/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.example.hw3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.hw3.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var username : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            if(binding.username.text.toString() == "user" && binding.password.text.toString() == "123456"){
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# CS501-Assignment3
